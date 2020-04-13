8c1363d5bb6665b4e3ec07859e0e936e7e984d7b
RobertoLovece <48356710+RobertoLovece@users.noreply.github.com>
2020-04-13 15:28:59 +0100

    Add files via upload

---
+++ node_intro/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+var app = express();
+var router = require('./routes/hello.js');
+
+app.use('/',router);
+
+var port = 3000;
+app.listen(port,function() {
+    console.log('Listening on port ' + port);
+});
+
